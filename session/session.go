2cbce6e9936040154bbb22fa95aa7b10bca23f68
Samer Masterson <samer@samertm.com>
2014-07-14 00:22:20 -0400

    Added ability to change cookie name

--- session/session.go
+++ session/session.go
@@ -8,16 +8,22 @@ import (
 
 // Session keeps track of the map between session ids and interface
 
+// TODO should I make Session unexported, only to be created with the
+// New() function?
 type Session struct {
 	mut *sync.Mutex
 	// waiting for generics
 	session map[string]interface{}
+	// sets the cookie name
+	// set to "sessionid" in New function
+	CookieName string
 }
 
 func New() *Session {
 	return &Session{
-		mut:     &sync.Mutex{},
-		session: make(map[string]interface{}),
+		mut:        &sync.Mutex{},
+		session:    make(map[string]interface{}),
+		CookieName: "sessionid",
 	}
 }
 
@@ -33,7 +39,7 @@ func (s *Session) Get(id string) (val interface{}, ok bool) {
 // grabs the cookie from the request to set the session
 // currently looks only for "sessionid"
 func (s *Session) CookieSet(r *http.Request, val interface{}) error {
-	c, err := r.Cookie("sessionid")
+	c, err := r.Cookie(s.CookieName)
 	if err != nil {
 		return errors.New("No cookie set")
 	}
